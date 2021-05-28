d57c847413fc5396b15fd6b375f8c64c9eeca194
MassiveBattlebotsFan <teamlightning2018@outlook.com>
2021-05-28 02:11:08 -0500

    doubled tape size

--- PLcompiler.py
+++ PLcompiler.py
@@ -1,7 +1,7 @@
 import sys
 if len(sys.argv) < 2:
     exit("Syntax: PLcompiler.py <program.ow>")
-#tape = [0,0,0,0,0,0,0,0]
+#tape = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 #index = 0
 cmds = {"o" : "index = (0 if index + 1 > 8 else index + 1); ", "w" : "tape[index] = (0 if tape[index] + 1 > 0b11111111 else tape[index] + 1); ", "!" : "sys.stdout.write(chr(tape[index])); ", "?" : "tape[index] = ord(sys.stdin.read(1)); ", "O" : "\nif tape[index] != 0:\n    while True: \n        ", "W" : "\n        if tape[index] == 0: break; \n", "A" : "index = 0; ", "a" : "tape[index] = 0; "}
 prog = ""
@@ -11,5 +11,5 @@ with open(sys.argv[1], "r") as file:
         if char in cmds.keys():
             prog += cmds.get(char)
 with open(sys.argv[1].split(".")[0]+".py", "w") as file:
-    file.write("import sys\ntape = [0,0,0,0,0,0,0,0]; index = 0; "+prog)
+    file.write("import sys\ntape = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]; index = 0; "+prog)
 print("script compiled")
