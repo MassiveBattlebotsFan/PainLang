f95c89920e289a58f9d9a64248c2f5feaf779a92
MassiveBattlebotsFan <teamlightning2018@outlook.com>
2021-05-28 10:42:25 -0500

    (Breaking change) updated standard, added debug flag to compiler

--- PLcompiler.py
+++ PLcompiler.py
@@ -1,15 +1,16 @@
 import sys
 if len(sys.argv) < 2:
-    exit("Syntax: PLcompiler.py <program.ow>")
+    exit("Syntax: PLcompiler.py <program.ow> [-d]")
 #tape = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 #index = 0
-cmds = {"o" : "index = (0 if index + 1 > 8 else index + 1); ", "w" : "tape[index] = (0 if tape[index] + 1 > 0b11111111 else tape[index] + 1); ", "!" : "sys.stdout.write(chr(tape[index])); ", "?" : "tape[index] = ord(sys.stdin.read(1)); ", "O" : "\nif tape[index] != 0:\n    while True: \n        ", "W" : "\n        if tape[index] == 0: break; \n", "A" : "index = 0; ", "a" : "tape[index] = 0; "}
+#neg = 1
+cmds = {"o" : "index = ((0 if index + 1 > 15 else index + 1) if neg == 1 else (15 if index - 1 < 0 else index - 1)); ", "w" : "tape[index] = ((0 if tape[index] + 1 > 0b11111111 else tape[index] + 1) if neg == 1 else (0b11111111 if tape[index] - 1 < 0b00000000 else tape[index] - 1)); ", "!" : "sys.stdout.write(chr(tape[index])); ", "?" : "tape[index] = ord(sys.stdin.read(1)); ", "O" : "\nif tape[index] != 0:\n    while True: \n        ", "W" : "\n        if tape[index] == 0: break; \n", "A" : "neg = 1; ", "a" : "neg = -1; "}
 prog = ""
 with open(sys.argv[1], "r") as file:
     data = file.read()
     for char in data:
         if char in cmds.keys():
-            prog += cmds.get(char)
+            prog += cmds.get(char)+("sys.stdout.write(str(tape)+\",\"+str(index)+\",\"+str(neg)); " if len(sys.argv)>2 and sys.argv[2]=="-d" else "")
 with open(sys.argv[1].split(".")[0]+".py", "w") as file:
-    file.write("import sys\ntape = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]; index = 0; "+prog)
+    file.write("import sys\ntape = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]; index = 0; neg = 1; "+prog)
 print("script compiled")
--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 A programming language whose commands are based off of the words "ow?", "OW!", and "AAAAAAAAAAAAAAAAA"  
 Loosely based off of Brainfuck, file extension is `.pain`.
 # Standard  
-Given an index `i` and a sixteen cell tape `t`, with each cell being one byte:  
+Given an index `i`, a negation bit `neg`, and a sixteen cell tape `t`, with each cell being one byte:  
 `o` sets `i` to `(i + 1) % 8`  
 `w` increments `t[i]`, and wraps to zero upon exceeding one byte  
 `!` prints `t[i]`  
 `?` reads one character from the console into `t[i]`  
 `O` jumps to the matching `W` character if `t[i]` is zero  
 `W` jumps to the matching `O` character if `t[i]` is nonzero  
-`a` sets `t[i]` to zero  
-`A` sets `i` to zero  
+`a` sets `neg` to -1  
+`A` sets `neg` to 1  
 Please don't nest loops
